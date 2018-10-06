a3e30601b9d0ffa0a15e51202bd2858e51209055
JON <jonn.thinkrtc@gmail.com>
2018-10-05 21:31:15 -0400

    updated index view to display list of access_records

--- first_app/views.py
+++ first_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
 
 # Create your views here.
 
 def index(request):
-    my_dic = {'insert_me': "Hello I am from views.py"}
-    return render(request, 'first_app/index.html', context=my_dic)
+    weblist = AccessRecord.objects.order_by('date')
+    date_dic = {'access_records': weblist}
+    return render(request, 'first_app/index.html', context=date_dic)
